c0b5e7ed592421c22b4d0fe9e1a3384d5be1bff0
Tim Janney <tim.s.janney@gmail.com>
2015-06-16 15:20:13 +0000

    layout improvements

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About #{title_end}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
+
 end
